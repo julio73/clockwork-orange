c010a2de1576e9e0249388217637e9995d28026f
Julio Maniratunga <mjb73w@gmail.com>
2015-07-21 01:36:08 +0200

    Correcting watch markings and switching gcd roundable values

--- js/main.js
+++ js/main.js
@@ -1,31 +1,43 @@
 /*global window, Snap */
 window.onload = function () {
   'use strict';
-  var scene, hours, hourText, hourPosX, faceWidth, spacing, markingsPath, watchFace;
+  var scene, hours, hourText, hourPosY, faceWidth, spacing, markingsPath,
+    intervals, watchFace;
 
   scene = new Snap().attr({id: "#scene"});
   hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-  faceWidth = 800;
-  spacing = Math.round(faceWidth / hours.length);
+  faceWidth = 720;
+  spacing = faceWidth / hours.length;
 
   // Display some hours
-  hourPosX = 100;
-  hourText = scene.text(0, hourPosX, hours).attr({
-    font: "300 30px Courier New",
+  hourPosY = 100;
+  hourText = scene.text(0, hourPosY, hours).attr({
+    font: "300 24px Courier New",
     textAnchor: "middle"
   });
 
   // Evenly space out the hours and build marking path
   markingsPath = "";
+  intervals = {
+    0: 0,
+    1: spacing / 6,
+    2: spacing * 2 / 6,
+    3: spacing * 3 / 6,
+    4: spacing * 4 / 6,
+    5: spacing * 5 / 6
+  };
   hours.forEach(function (hour, index) {
     var posx = spacing * hour;
     // Select hour and move it
     hourText.select("tspan:nth-child(" + (index + 1) + ")")
       .attr({x: posx});
     // Build marking path
-    markingsPath += "M" + [posx, 0, posx, 20]
-      + "M" + [posx + spacing * 0.333, 10, posx + spacing * 0.333, 20]
-      + "M" + [posx + spacing * 0.666, 10, posx + spacing * 0.666, 20];
+    markingsPath += "M" + [posx - intervals[0], hourPosY + 15, posx - intervals[0], hourPosY + 30]
+      + "M" + [posx - intervals[1], hourPosY + 27, posx - intervals[1], hourPosY + 30]
+      + "M" + [posx - intervals[2], hourPosY + 27, posx - intervals[2], hourPosY + 30]
+      + "M" + [posx - intervals[3], hourPosY + 21, posx - intervals[3], hourPosY + 30]
+      + "M" + [posx - intervals[4], hourPosY + 27, posx - intervals[4], hourPosY + 30]
+      + "M" + [posx - intervals[5], hourPosY + 27, posx - intervals[5], hourPosY + 30];
   });
 
   // Add the markings and group them with hours under the watch face
@@ -36,9 +48,6 @@ window.onload = function () {
         fill: "none",
         stroke: "#000",
         strokeWidth: 1
-      })
-      .attr({
-        transform: "t" + [0, hourPosX + 10]
       })
   );
 };
