bf9cfa2971bb9f950429064ecc2d62ca8ed80be5
Julio Maniratunga <mjb73w@gmail.com>
2015-07-20 23:17:11 +0200

    Adding markings under the hours

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /*global window, Snap */
 window.onload = function () {
   'use strict';
-  var scene, hours, watch, faceWidth, spacing;
+  var scene, hours, hourText, hourPosX, faceWidth, spacing, markingsPath, watchFace;
 
   scene = new Snap().attr({id: "#scene"});
   hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
@@ -9,16 +9,36 @@ window.onload = function () {
   spacing = Math.round(faceWidth / hours.length);
 
   // Display some hours
-  watch = scene.text(0, 100, hours).attr({
+  hourPosX = 100;
+  hourText = scene.text(0, hourPosX, hours).attr({
     font: "300 30px Courier New",
     textAnchor: "middle"
   });
 
-  // Evenly space out the hours
+  // Evenly space out the hours and build marking path
+  markingsPath = "";
   hours.forEach(function (hour, index) {
-    watch.select("tspan:nth-child(" + (index + 1) + ")")
-      .attr({
-        x: spacing * hour
-      });
+    var posx = spacing * hour;
+    // Select hour and move it
+    hourText.select("tspan:nth-child(" + (index + 1) + ")")
+      .attr({x: posx});
+    // Build marking path
+    markingsPath += "M" + [posx, 0, posx, 20]
+      + "M" + [posx + spacing * 0.333, 10, posx + spacing * 0.333, 20]
+      + "M" + [posx + spacing * 0.666, 10, posx + spacing * 0.666, 20];
   });
+
+  // Add the markings and group them with hours under the watch face
+  watchFace = scene.group(
+    hourText,
+    scene.path(markingsPath)
+      .attr({
+        fill: "none",
+        stroke: "#000",
+        strokeWidth: 1
+      })
+      .attr({
+        transform: "t" + [0, hourPosX + 10]
+      })
+  );
 };
